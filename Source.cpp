8bdd1ce994f4b10792d08a369ec62887fed87834
jonttufin <32910370+Joona70@users.noreply.github.com>
2020-05-27 07:35:39 +0300

    updated for newest gamepatch

--- Source.cpp
+++ Source.cpp
@@ -7,10 +7,7 @@
 
 #define M_PI 3.14159265358979323846264338327950288419716939937510
 
-#define OFFSET_UWORLD 0x748CC10 //48 8b 0d ? ? ? ? 48 85 c9 74 30 e8 ? ? ? ? 48 8b f8
-#define location_Offs 0x74671E0 //F3 44 0F 11 1D ? ? ? ?
-
-#define botid 18388801
+#define OFFSET_UWORLD 0x8345058
 
 bool Menu = false;
 bool Aimbot = true;
@@ -42,14 +39,14 @@ DWORD_PTR Ulevel;
 Vector3 localactorpos;
 Vector3 Localcam;
 
-float AimFOV = 20; 
+float AimFOV = 120.f; 
 
 bool isaimbotting;
 DWORD_PTR entityx;
 
 FTransform GetBoneIndex(DWORD_PTR mesh, int index)
 {
-	DWORD_PTR bonearray = read<DWORD_PTR>(DriverHandle, processID, mesh + 0x410);
+	DWORD_PTR bonearray = read<DWORD_PTR>(DriverHandle, processID, mesh + 0x420);
 
 	return read<FTransform>(DriverHandle, processID, bonearray + (index * 0x30));
 }
@@ -102,31 +99,49 @@ D3DXMATRIX Matrix(Vector3 rot, Vector3 origin = Vector3(0, 0, 0))
 	return matrix;
 }
 
-Vector3 ProjectWorldToScreen(Vector3 WorldLocation, Vector3 camrot)
+Vector3 ProjectWorldToScreen(Vector3 WorldLocation)
 {
 	Vector3 Screenlocation = Vector3(0, 0, 0);
-	Vector3 Rotation = camrot; // FRotator
+	Vector3 Camera;
 
-	D3DMATRIX tempMatrix = Matrix(Rotation);
+	auto Nigger = read<uintptr_t>(DriverHandle, processID, Localplayer + 0xa8);
+	uint64_t Nigger1 = read<uintptr_t>(DriverHandle, processID, Nigger + 8);
 
+	Camera.x = read<float>(DriverHandle, processID, Nigger1 + 0x678);
+	Camera.y = read<float>(DriverHandle, processID, Rootcomp + 0x12C);
+
+	float test = asin(Camera.x);
+	float degrees = test * (180.0 / M_PI);
+	Camera.x = degrees;
+
+	if (Camera.y < 0)
+		Camera.y = 360 + Camera.y;
+
+	D3DMATRIX tempMatrix = Matrix(Camera);
 	Vector3 vAxisX, vAxisY, vAxisZ;
 
 	vAxisX = Vector3(tempMatrix.m[0][0], tempMatrix.m[0][1], tempMatrix.m[0][2]);
 	vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
 	vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
 
-	Vector3 camloc = read<Vector3>(DriverHandle, processID, base_address + location_Offs);
+	/*world->owningGameInstance->playersArray->localPlayer + 0x70] + 0x98] + 0x130] + 0x10
+	world->owningGameInstance->playersArray->localPlayer + 0x20] + 0xAB0] + 0x130] + 0x10
+	world->persistentLevel + 0x20] + 0x178] + 0x130] + 0x10
+	world->persistentLevel + 0xB8] + 0x178] + 0x130] + 0x10*/
 
-	Vector3 vDelta = WorldLocation - camloc;
+	uint64_t chain = read<uint64_t>(DriverHandle, processID, Ulevel + 0x20);
+	uint64_t chain1 = read<uint64_t>(DriverHandle, processID, chain + 0x178);
+	uint64_t chain2 = read<uint64_t>(DriverHandle, processID, chain1 + 0x130);
+
+	Vector3 vDelta = WorldLocation - read<Vector3>(DriverHandle, processID, chain2 + 0x10); //camera location credits for Object9999
 	Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX));
 
 	if (vTransformed.z < 1.f)
 		vTransformed.z = 1.f;
 
-	uint64_t zoomBase = read<uint64_t>(DriverHandle, processID, Localplayer + 0xb0);
-	float zoom = read<float>(DriverHandle, processID, zoomBase + 0x500);
+	float zoom = read<float>(DriverHandle, processID, Nigger1 + 0x500);
 
-	FovAngle = 80.0f / (zoom / 1.19f);
+	float FovAngle = 80.0f / (zoom / 1.19f);
 	float ScreenCenterX = width / 2.0f;
 	float ScreenCenterY = height / 2.0f;
 
@@ -135,26 +150,6 @@ Vector3 ProjectWorldToScreen(Vector3 WorldLocation, Vector3 camrot)
 
 	return Screenlocation;
 }
-
-Vector3 Camera(unsigned __int64 RootComponent)
-{
-	Vector3 Camera;
-
-	auto pitch = read<uintptr_t>(DriverHandle, processID, Localplayer + 0xb0);
-	Camera.x = read<float>(DriverHandle, processID, RootComponent + 0x12C);
-	Camera.y = read<float>(DriverHandle, processID, pitch + 0x678);
-
-	float test = asin(Camera.y);
-	float degrees = test * (180.0 / M_PI);
-
-	Camera.y = degrees;
-
-	if (Camera.x < 0)
-		Camera.x = 360 + Camera.x;
-
-	return Camera;
-}
-
 
 void menu()
 {
@@ -251,11 +246,6 @@ DWORD Menuthread(LPVOID in)
 	}
 }
 
-bool GetAimKey()
-{
-	return (GetAsyncKeyState(VK_MENU));
-}
-
 void aimbot(float x, float y)
 {
 	float ScreenCenterX = (width / 2);
@@ -303,106 +293,35 @@ void aimbot(float x, float y)
 	return;
 }
 
-double GetCrossDistance(double x1, double y1, double x2, double y2)
-{
-	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-}
-
-bool GetClosestPlayerToCrossHair(Vector3 Pos, float& max, float aimfov, DWORD_PTR entity)
-{
-	if (!GetAimKey() || !isaimbotting)
-	{
-		if (entity)
-		{
-			float Dist = GetCrossDistance(Pos.x, Pos.y, width / 2, height / 2);
-
-			if (Dist < max)
-			{
-				max = Dist;
-				entityx = entity;
-				AimFOV = aimfov;
-			}
-		}
-	}
-	return false;
-}
-
-void AimAt(DWORD_PTR entity, Vector3 Localcam)
+void AimAt(DWORD_PTR entity)
 {
 	uint64_t currentactormesh = read<uint64_t>(DriverHandle, processID, entity + 0x278);
 	auto rootHead = GetBoneWithRotation(currentactormesh, 66);
-	Vector3 rootHeadOut = ProjectWorldToScreen(rootHead, Vector3(Localcam.y, Localcam.x, Localcam.z));
+	Vector3 rootHeadOut = ProjectWorldToScreen(rootHead);
 
 	if (rootHeadOut.y != 0 || rootHeadOut.y != 0)
 	{
-		if ((GetCrossDistance(rootHeadOut.x, rootHeadOut.y, width / 2, height / 2) <= AimFOV * 8) || isaimbotting)
-		{
-			aimbot(rootHeadOut.x, rootHeadOut.y);
-		}
+		aimbot(rootHeadOut.x, rootHeadOut.y);
 	}
-}
-
-void aimbot(Vector3 Localcam)
-{
-	if (entityx != 0)
-	{
-		if (GetAimKey())
-		{
-			AimAt(entityx, Localcam);
-		}
-		else
-		{
-			isaimbotting = false;
-		}
-	}
-}
-
-void AIms(DWORD_PTR entity, Vector3 Localcam)
-{
-	float max = 100.f;
-
-	uint64_t currentactormesh = read<uint64_t>(DriverHandle, processID, entity + 0x278);
-
-	Vector3 rootHead = GetBoneWithRotation(currentactormesh, 66);
-	Vector3 rootHeadOut = ProjectWorldToScreen(rootHead, Vector3(Localcam.y, Localcam.x, Localcam.z));
-
-	if (GetClosestPlayerToCrossHair(rootHeadOut, max, AimFOV, entity))
-		entityx = entity;
 }
 
 void DrawSkeleton(DWORD_PTR mesh)
 {
-	Vector3 vHeadBone = GetBoneWithRotation(mesh, 96);
-	Vector3 vHip = GetBoneWithRotation(mesh, 2);
-	Vector3 vNeck = GetBoneWithRotation(mesh, 65);
-	Vector3 vUpperArmLeft = GetBoneWithRotation(mesh, 34);
-	Vector3 vUpperArmRight = GetBoneWithRotation(mesh, 91);
-	Vector3 vLeftHand = GetBoneWithRotation(mesh, 35);
-	Vector3 vRightHand = GetBoneWithRotation(mesh, 63);
-	Vector3 vLeftHand1 = GetBoneWithRotation(mesh, 33);
-	Vector3 vRightHand1 = GetBoneWithRotation(mesh, 60);
-	Vector3 vRightThigh = GetBoneWithRotation(mesh, 74);
-	Vector3 vLeftThigh = GetBoneWithRotation(mesh, 67);
-	Vector3 vRightCalf = GetBoneWithRotation(mesh, 75);
-	Vector3 vLeftCalf = GetBoneWithRotation(mesh, 68);
-	Vector3 vLeftFoot = GetBoneWithRotation(mesh, 69);
-	Vector3 vRightFoot = GetBoneWithRotation(mesh, 76);
-
-	Vector3 vHeadBoneOut = ProjectWorldToScreen(vHeadBone, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vHipOut = ProjectWorldToScreen(vHip, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vNeckOut = ProjectWorldToScreen(vNeck, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vUpperArmLeftOut = ProjectWorldToScreen(vUpperArmLeft, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vUpperArmRightOut = ProjectWorldToScreen(vUpperArmRight, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vLeftHandOut = ProjectWorldToScreen(vLeftHand, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vRightHandOut = ProjectWorldToScreen(vRightHand, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vLeftHandOut1 = ProjectWorldToScreen(vLeftHand1, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vRightHandOut1 = ProjectWorldToScreen(vRightHand1, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vRightThighOut = ProjectWorldToScreen(vRightThigh, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vLeftThighOut = ProjectWorldToScreen(vLeftThigh, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vRightCalfOut = ProjectWorldToScreen(vRightCalf, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vLeftCalfOut = ProjectWorldToScreen(vLeftCalf, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vLeftFootOut = ProjectWorldToScreen(vLeftFoot, Vector3(Localcam.y, Localcam.x, Localcam.z));
-	Vector3 vRightFootOut = ProjectWorldToScreen(vRightFoot, Vector3(Localcam.y, Localcam.x, Localcam.z));
+	Vector3 vHeadBoneOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 96));
+	Vector3 vHipOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 2));
+	Vector3 vNeckOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 65));
+	Vector3 vUpperArmLeftOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 34));
+	Vector3 vUpperArmRightOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 91));
+	Vector3 vLeftHandOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 35));
+	Vector3 vRightHandOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 63));
+	Vector3 vLeftHandOut1 = ProjectWorldToScreen(GetBoneWithRotation(mesh, 33));
+	Vector3 vRightHandOut1 = ProjectWorldToScreen(GetBoneWithRotation(mesh, 60));
+	Vector3 vRightThighOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 74));
+	Vector3 vLeftThighOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 67));
+	Vector3 vRightCalfOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 75));
+	Vector3 vLeftCalfOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 68));
+	Vector3 vLeftFootOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 69));
+	Vector3 vRightFootOut = ProjectWorldToScreen(GetBoneWithRotation(mesh, 76));
 
 	DrawLine(vHipOut.x, vHipOut.y, vNeckOut.x, vNeckOut.y, 2.f, 255.f, 0.f, 0.f, 200.f);
 
@@ -428,17 +347,12 @@ void DrawSkeleton(DWORD_PTR mesh)
 void drawLoop(int width, int height) {
 	menu();
 
-	float radiusx = AimFOV * (width / 2 / 100);
-	float radiusy = AimFOV * (height / 2 / 100);
-
-	float calcradius = (radiusx + radiusy) / 2;
-
-	DrawCircle(width / 2, height / 2, calcradius, 2.f, 255.f, 0.f, 0.f, 255.f, false);
+	DrawCircle(width / 2, height / 2, AimFOV, 2.f, 255.f, 0.f, 0.f, 255.f, false);
 
 	Uworld = read<DWORD_PTR>(DriverHandle, processID, base_address + OFFSET_UWORLD);
 	//printf(_xor_("Uworld: %p.\n").c_str(), Uworld);
 
-	DWORD_PTR Gameinstance = read<DWORD_PTR>(DriverHandle, processID, Uworld + 0x170);
+	DWORD_PTR Gameinstance = read<DWORD_PTR>(DriverHandle, processID, Uworld + 0x188);
 
 	if (Gameinstance == (DWORD_PTR)nullptr)
 		return;
@@ -503,13 +417,16 @@ void drawLoop(int width, int height) {
 	if (AActors == (DWORD_PTR)nullptr)
 		return;
 
+	float closestDistance = FLT_MAX;
+	DWORD_PTR closestPawn = NULL;
+
 	for (int i = 0; i < ActorCount; i++)
 	{
 		uint64_t CurrentActor = read<uint64_t>(DriverHandle, processID, AActors + i * 0x8);
 
 		int curactorid = read<int>(DriverHandle, processID, CurrentActor + 0x18);
 
-		if (curactorid == localplayerID || curactorid == 18388801)
+		if (curactorid == localplayerID || curactorid == localplayerID + 121)
 		{
 			if (CurrentActor == (uint64_t)nullptr || CurrentActor == -1 || CurrentActor == NULL)
 				continue;
@@ -524,36 +441,32 @@ void drawLoop(int width, int height) {
 			if (currentactormesh == (uint64_t)nullptr || currentactormesh == -1 || currentactormesh == NULL)
 				continue;
 
-			int MyTeamId = read<int>(DriverHandle, processID, PlayerState + 0xE50);
+			int MyTeamId = read<int>(DriverHandle, processID, PlayerState + 0xE60);
 
 			DWORD64 otherPlayerState = read<uint64_t>(DriverHandle, processID, CurrentActor + 0x238);
 
 			if (otherPlayerState == (uint64_t)nullptr || otherPlayerState == -1 || otherPlayerState == NULL)
 				continue;
 
-			int ActorTeamId = read<int>(DriverHandle, processID, otherPlayerState + 0xE50);
-
-			Vector3 Headpos = GetBoneWithRotation(currentactormesh, 66);
-			Localcam = Camera(Rootcomp);
-			localactorpos = read<Vector3>(DriverHandle, processID, Rootcomp + 0x11C);
-
-			float distance = localactorpos.Distance(Headpos) / 100.f;
-
-			if (distance < 1.5f)
-				continue;
-
-			//W2S
-			Vector3 HeadposW2s = ProjectWorldToScreen(Headpos, Vector3(Localcam.y, Localcam.x, Localcam.z));
-			Vector3 bone0 = GetBoneWithRotation(currentactormesh, 0);
-			Vector3 bottom = ProjectWorldToScreen(bone0, Vector3(Localcam.y, Localcam.x, Localcam.z));
-			Vector3 Headbox = ProjectWorldToScreen(Vector3(Headpos.x, Headpos.y, Headpos.z + 15), Vector3(Localcam.y, Localcam.x, Localcam.z));
-			Vector3 Aimpos = ProjectWorldToScreen(Vector3(Headpos.x, Headpos.y, Headpos.z + 10), Vector3(Localcam.y, Localcam.x, Localcam.z));
-
-			float Height1 = abs(Headbox.y - bottom.y);
-			float Width1 = Height1 * 0.65;
+			int ActorTeamId = read<int>(DriverHandle, processID, otherPlayerState + 0xE60);
 
 			if (MyTeamId != ActorTeamId)
 			{
+				Vector3 Headpos = GetBoneWithRotation(currentactormesh, 66);
+				localactorpos = read<Vector3>(DriverHandle, processID, Rootcomp + 0x11C);
+
+				float distance = localactorpos.Distance(Headpos) / 100.f;
+
+				//W2S
+				Vector3 HeadposW2s = ProjectWorldToScreen(Headpos);
+				Vector3 bone0 = GetBoneWithRotation(currentactormesh, 0);
+				Vector3 bottom = ProjectWorldToScreen(bone0);
+				Vector3 Headbox = ProjectWorldToScreen(Vector3(Headpos.x, Headpos.y, Headpos.z + 15));
+				Vector3 Aimpos = ProjectWorldToScreen(Vector3(Headpos.x, Headpos.y, Headpos.z + 10));
+
+				float Height1 = abs(Headbox.y - bottom.y);
+				float Width1 = Height1 * 0.65;
+
 				if (skeleton)
 					DrawSkeleton(currentactormesh);
 
@@ -567,7 +480,6 @@ void drawLoop(int width, int height) {
 				{
 					CHAR dist[50];
 					sprintf_s(dist, _xor_("[%.f]").c_str(), distance);
-
 					DrawString(dist, 13, HeadposW2s.x + 40, HeadposW2s.y - 25, 0, 1, 1);
 				}
 
@@ -576,7 +488,14 @@ void drawLoop(int width, int height) {
 
 				if (Aimbot)
 				{
-					AIms(CurrentActor, Localcam);
+					auto dx = HeadposW2s.x - (width / 2);
+					auto dy = HeadposW2s.y - (height / 2);
+					auto dist = sqrtf(dx * dx + dy * dy);
+
+					if (dist < AimFOV && dist < closestDistance) {
+						closestDistance = dist;
+						closestPawn = CurrentActor;
+					}
 				}
 			}
 		}
@@ -584,7 +503,9 @@ void drawLoop(int width, int height) {
 
 	if (Aimbot)
 	{
-		aimbot(Localcam);
+		if (closestPawn && GetAsyncKeyState(0x05) < 0) { //change aimkey here
+			AimAt(closestPawn);
+		}
 	}
 }
 
